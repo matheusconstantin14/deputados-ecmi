0690dcdabb0ee247b2f95a048b7767dde29148c8
Matheus Constantin Rodrigues de Souza <matheusconstantin14@gmail.com>
2026-05-05 18:19:15 +0000

    erver

--- deputados.py
+++ deputados.py
@@ -5,7 +5,7 @@ df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
 
 
-partido = st.text_input("Escolha a sigla do Partido: ")
+sigla = st.text_input("Escolha a sigla do Partido: ")
 uf = st.text_input('Digite a UF')
 
 if sigla:
